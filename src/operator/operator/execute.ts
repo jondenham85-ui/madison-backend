e3d2e95ed5cd10da6309c42de63a414c69b9170c
jondenham85-ui <jondenham85@gmail.com>
2026-09-26 02:53:17 -0500

    Delete src/operator/operator directory

--- src/operator/operator/execute.ts
+++
@@ -1,8 +0,0 @@
-export default async function executeCode(code: string) {
-  try {
-    const result = await eval(code);
-    return { success: true, result };
-  } catch (err: any) {
-    return { success: false, error: err.toString() };
-  }
-}
